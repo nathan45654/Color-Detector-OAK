7f2aa57951439c97646fac1a55d576a450ea07e6
Nathan Jones <nathanij@byu.edu>
2023-07-18 11:59:54 -0600

    add stream-every-n

--- entry.sh
+++ entry.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/bootstrap.sh $DIR $DIR/venv
 
-$DIR/venv/bin/python $DIR/src/main.py $@ --camera-port 50052 --canbus-port 50060
+$DIR/venv/bin/python $DIR/src/main.py $@ --camera-port 50052 --canbus-port 50060 --stream-every-n 2
 
 exit 0
