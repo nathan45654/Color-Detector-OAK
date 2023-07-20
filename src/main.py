9b7c68e0616f28a7e45822ede699077909203055
Nathan Jones <nathanij@byu.edu>
2023-07-20 12:39:23 -0600

    edit

--- src/main.py
+++ src/main.py
@@ -293,21 +293,21 @@ class CameraColorApp(App):
                         
                         # #######
                         # # put text and highlight the center
-                        # if cX and cY:
-                        #     cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
-                        # # cv2.putText(purple_result, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        # #######    
-                        
-                        disparity_img = self.image_decoder.decode(
-                            getattr(frame, "disparity").image_data
-                        )
-                        # disparity_img = cv2.resize(disparity_img,(img.shape[1], img.shape[0]))
-                        #-----#
-                        # put text and highlight the center
                         if cX and cY:
                             cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
-                            text = "Center: " + str(disparity_img[10][10][1])
-                            cv2.putText(img, text, (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                            cv2.putText(img, "centroid: " + cX + cY, (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                        # #######    
+                        
+                        # disparity_img = self.image_decoder.decode(
+                        #     getattr(frame, "disparity").image_data
+                        # )
+                        # # disparity_img = cv2.resize(disparity_img,(img.shape[1], img.shape[0]))
+                        # #-----#
+                        # # put text and highlight the center
+                        # if cX and cY:
+                        #     cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+                        #     text = "Center: " + str(disparity_img[10][10][1])
+                        #     cv2.putText(img, text, (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                         #-----#
                         
                     elif view_name == "disparity":
